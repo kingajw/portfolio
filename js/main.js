639922f93841c71b34f7d8de106e7122ec5248e9
Kinga <kinga.walaszek@protonmail.com>
2017-10-02 20:25:47 +0200

    Added all sections, including contact form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.next-section').click(function() {
         $('html,body').animate({
-            scrollTop: $("#about1").offset().top
+            scrollTop: $("#about-1").offset().top
         },700);
     });
 });
