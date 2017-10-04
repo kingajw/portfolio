0e9c9cf583c469cafa46c2ee4a04f55794579022
Kinga <kinga.walaszek@protonmail.com>
2017-10-04 22:14:22 +0200

    Added social media buttons, cosmetic changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-    $('.next-section').click(function() {
-        $('html,body').animate({
-            scrollTop: $("#about-1").offset().top
-        },700);
+    $(".main").onepage_scroll();
+
+    $("#scroll-down").on('click', function () {
+        var position = $("#about-me").offset().top;
+        $("html,body").animate({
+            scrollTop: position}, 700);
     });
 });
 
-$( document ).ready(function() {
-    $(".main").onepage_scroll();
-});
