4051bb06e6b9c0289c485b82dc8b80ea7c9835e9
Kinga <kinga.walaszek@protonmail.com>
2017-10-09 10:30:29 +0200

    Added Pogoda to projects

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
     $(".main").onepage_scroll();
-
-    $("#scroll-down").on('click', function () {
-        var position = $("#about-me").offset().top;
-        $("html,body").animate({
-            scrollTop: position}, 700);
-    });
 });
 
